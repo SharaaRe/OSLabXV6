9e4083afde9f4371b205e20b54d32e883b9e0d0a
Sharaa_re <norouzi.sharare@gmail.com>
2020-02-17 15:20:25 +0330

    Added booting message not sure about the place of message

--- src/init.c
+++ src/init.c
@@ -7,6 +7,17 @@
 
 char *argv[] = { "sh", 0 };
 
+void 
+bootmsg(){
+  printf(1, "--------------------------------- \n");
+  printf(1, "--------- Group Members --------- \n");
+  printf(1, "- 1. Mohammad Rabiei: 810195395 - \n");
+  printf(1, "- 2. Sharare Norouzi: 810195395 - \n");
+  printf(1, "- 3. Ali Ranjbar    : 810195395 - \n");
+  printf(1, "--------------------------------- \n");
+}
+
+
 int
 main(void)
 {
@@ -18,14 +29,18 @@ main(void)
   }
   dup(0);  // stdout
   dup(0);  // stderr
+  bootmsg();
+
 
   for(;;){
     printf(1, "init: starting sh\n");
+
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
       exit();
     }
+
     if(pid == 0){
       exec("sh", argv);
       printf(1, "init: exec sh failed\n");
