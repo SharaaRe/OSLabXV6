b945978d759731580f6c2c1662ffe14f680ac05e
Ali Ranjbar <ali1377ranjbar@gmail.com>
2020-06-12 23:28:26 +0430

    Add nullread and change O flag

---
+++ src/nullread.c
@@ -0,0 +1,9 @@
+#include "types.h"
+#include "user.h"
+
+int main(int argc, char **argv) {
+    void *null_pointer = 0;
+    int null_pointer_content = *(int*)null_pointer;
+    printf(1, "NULL pointer content: 0x%x\n", null_pointer_content);
+    printf(1, "0:	8d 4c 24 04     lea ecx, [esp+0x4]\n");
+}
