be3cc71df9bbcb8dfa522a2487b0321b57108951
Ali Ranjbar <ali1377ranjbar@gmail.com>
2020-06-12 23:49:10 +0430

    Very minor edit

--- src/nullread.c
+++ src/nullread.c
@@ -1,9 +1,11 @@
 #include "types.h"
 #include "user.h"
+#define NULL ((void*) 0)
 
 int main(int argc, char **argv) {
-    void *null_pointer = 0;
+    void *null_pointer = NULL;
     int null_pointer_content = *(int*)null_pointer;
     printf(1, "NULL pointer content: 0x%x\n", null_pointer_content);
     printf(1, "0:	8d 4c 24 04     lea ecx, [esp+0x4]\n");
+    exit();
 }
